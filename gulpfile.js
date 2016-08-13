11284983b0ed0f93761accc7dccec6f5f54748e0
Kevin Tantisevi <pongpanot.t@nexts-corp.com>
2016-08-13 16:29:44 +0700

    try

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var vulcanize = require('gulp-vulcanize');
+var concat = require('gulp-concat');
 
 gulp.task('vulcanize',function(){
   return gulp.src('src/import.html')
@@ -11,4 +12,24 @@ gulp.task('vulcanize',function(){
   .pipe(gulp.dest('src/dist'));
 });
 
-gulp.task('default', ['vulcanize']);
+gulp.task('html', function() {
+return gulp.src([
+    'bower_components/paper-drawer-panel/paper-drawer-panel.html',
+    'bower_components/paper-header-panel/paper-header-panel.html', 
+    'bower_components/paper-toolbar/paper-toolbar.html'
+])
+.pipe(concat('test.html'))
+.pipe(gulp.dest('./'));
+});
+
+gulp.task('vulcanize2',function(){
+  return gulp.src('im.html')
+  .pipe(vulcanize({
+    stripComments:true,
+    inlineScripts:true,
+    inlineCss:true
+  }))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['vulcanize2']);
