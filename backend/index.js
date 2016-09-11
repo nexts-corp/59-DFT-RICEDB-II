4246ecafe46331f12048c4a6547a1cf4185465a1
Kevin Tantisevi <pongpanot.t@nexts-corp.com>
2016-09-11 16:51:35 +0700

    test join data ok

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 var test = require('./routes/test');
+var contract = require('./routes/contact');
 
 app.use('/', test);
+app.use('/contract', contract);
 
 app.listen(3000);
