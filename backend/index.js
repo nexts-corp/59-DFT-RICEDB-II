e9feefb547f108ecb01deadf43fabbca8c883449
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2016-09-22 19:40:26 +0700

    edit set all Access-Control-Allow-Origin

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ var app = express();
 var test = require('./routes/test');
 var contract = require('./routes/contract');
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-  next();
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  return next();
 });
 
-app.use('/', test);
+app.use(test);
 app.use('/contract', contract);
 
 app.listen(3000);
