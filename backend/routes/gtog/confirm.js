938f96b7441d6d06477736675a92dcbacd73a0f5
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-09-23 15:40:13 +0700

    query confirm letter by id

--- backend/routes/gtog/confirm.js
+++ backend/routes/gtog/confirm.js
@@ -14,26 +14,20 @@ router.get('/:cl_id', function (req, res, next) {
                     cl_id: row('id'),
                     cl_type_rice: row('cl_type_rice').map(function (arr_type_rice) {
                         return arr_type_rice.merge(function (row_type_rice) {
-                            return r.table('type_rice')
-                                .get(row_type_rice('type_rice_id'))
-                                .without('id')
-                                // .merge(function (row_package) {
-                                //     return {
-                                //         package: row_package('package').map(function (arr_package) {
-                                //             return arr_package.merge(function (row_pack) {
-                                //                 return r.table('package')
-                                //                     .get(row_pack('package_id'))
-                                //                     .without('id')
-                                //             })
-                                //         })
-                                //     }
-                                // })
+                            return {
+                                package: row_type_rice('package').map(function (arr_package) {
+                                    return arr_package.merge(function (row_package) {
+                                        return r.table('package').get(row_package('package_id')).without('id')
+                                    })
+                                })
+                            }
+                        }).merge(function (row_type_rice) {
+                            return r.table('type_rice').get(row_type_rice('type_rice_id')).without('id')
                         })
                     })
                 }
             })
             .without('id')
-
             .run(conn, function (err, cursor) {
                 if (!err) {
                     res.json(cursor);
