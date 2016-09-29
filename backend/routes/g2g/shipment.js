4bca7bdb9c8a864338390e53b5075dd10204a3cd
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-09-29 18:19:33 +0700

    insert update delete [shipment]

--- backend/routes/g2g/shipment.js
+++ backend/routes/g2g/shipment.js
@@ -4,6 +4,27 @@ var router = express.Router();
 var r = require('rethinkdb');
 var db = require('../../db.js');
 
+var Ajv = require('ajv');
+var ajv = Ajv({ allErrors: true });
+var schema = {
+    //'type': 'object',
+    "properties": {
+        "id": {
+            "type": "string"
+        },
+        "contract_id": {
+            "type": "string"
+        },
+        "shm_no": {
+            "type": "string"
+        },
+        "shm_name": {
+            "type": "string"
+        }
+    },
+    "required": ["contract_id", "shm_no", "shm_name"]
+};
+var validate = ajv.compile(schema);
 
 router.get('/:shm_id', function (req, res, next) {
     db.query(function (conn) {
@@ -67,6 +88,113 @@ router.get('/:shm_id', function (req, res, next) {
                 }
             });
     })
+});
+router.post('/insert', function (req, res, next) {
+    //console.log(req.body);
+    var valid = validate(req.body);
+    var result = { result: false, message: null, id: null };
+    if (valid) {
+        //console.log(req.body);
+        if (req.body.id == null) {
+            //result.id = req.body.id;
+            db.query(function (conn) {
+                r.table("shipment")
+                    //.get(req.body.id)
+                    .insert(req.body)
+                    .run(conn)
+                    .then(function (response) {
+                        result.message = response;
+                        if (response.errors == 0) {
+                            result.result = true;
+                        }
+                        res.json(result);
+                        console.log(result);
+                    })
+                    .error(function (err) {
+                        result.message = err;
+                        res.json(result);
+                        console.log(result);
+                    })
+            })
+        } else {
+            result.message = 'field "id" must do not have data';
+            res.json(result);
+        }
+    } else {
+        result.message = ajv.errorsText(validate.errors);
+        res.json(result);
+    }
+});
+router.put('/update', function (req, res, next) {
+    //console.log(req.body);
+    var valid = validate(req.body);
+    var result = { result: false, message: null, id: null };
+    if (valid) {
+        //console.log(req.body);
+        if (req.body.id != '' || req.body.id != null) {
+            result.id = req.body.id;
+            db.query(function (conn) {
+                r.table("shipment")
+                    .get(req.body.id)
+                    .update(req.body)
+                    .run(conn)
+                    .then(function (response) {
+                        result.message = response;
+                        if (response.errors == 0) {
+                            result.result = true;
+                        }
+                        res.json(result);
+                        console.log(result);
+                    })
+                    .error(function (err) {
+                        result.message = err;
+                        res.json(result);
+                        console.log(result);
+                    })
+            })
+        } else {
+            result.message = 'require field id';
+            res.json(result);
+        }
+    } else {
+        result.message = ajv.errorsText(validate.errors);
+        res.json(result);
+    }
+});
+router.delete('/delete', function (req, res, next) {
+    //var valid = validate(req.body);
+    var result = { result: false, message: null, id: null };
+    //  if (valid) {
+    //console.log(req.body);
+    if (req.body.id != '' || req.body.id != null) {
+        // result.id = req.body.id;
+        db.query(function (conn) {
+            r.table("shipment")
+                .get(req.body.id)
+                .delete()
+                .run(conn)
+                .then(function (response) {
+                    result.message = response;
+                    if (response.errors == 0) {
+                        result.result = true;
+                    }
+                    res.json(result);
+                    console.log(result);
+                })
+                .error(function (err) {
+                    result.message = err;
+                    res.json(result);
+                    console.log(result);
+                })
+        })
+    } else {
+        result.message = 'require field id';
+        res.json(result);
+    }
+    // } else {
+    //     result.message = ajv.errorsText(validate.errors);
+    //     res.json(result);
+    // }
 });
 
 module.exports = router;
