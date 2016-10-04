92c612bcaadf250905091a0d234ad32ed1b998ed
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-10-04 11:58:07 +0700

    change file name confirm letter

--- backend/routes/g2g/index.js
+++ backend/routes/g2g/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 var contract = require('./contract');
-var confirm = require('./confirm');
+var confirmLetter = require('./confirmLetter');
 var shipment = require('./shipment');
 var shipmentDetail = require('./shipmentDetail');
 app.use('/contract', contract);
-app.use('/confirm', confirm);
+app.use('/confirm', confirmLetter);
 app.use('/shipment', shipment);
 app.use('/shipment/detail', shipmentDetail);
 
