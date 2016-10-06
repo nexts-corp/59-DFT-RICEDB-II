de38b7358192438979cf917572920cbde1f4aaf5
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-10-06 10:15:48 +0700

    add shm_det_quantity

--- backend/routes/g2g/shipment.js
+++ backend/routes/g2g/shipment.js
@@ -200,7 +200,54 @@ router.get('/:shm_id', function (req, res, next) {
                             .merge(function (row_shm_det) {
                                 return r.table('package').get(row_shm_det('package_id')).without("id")
                             })
-                    })
+                    }),
+                    shm_quantity: row('shipment_detail').sum("shm_det_quantity")
+                    // shipment_detail: r.table("shipment_detail")
+                    //     .filter({ "shm_id": row('id') })
+                    //     .orderBy(r.desc('shm_det_quantity'))
+                    //     .eqJoin("load_port_id", r.table("port")).map(function (port) {
+                    //         return port.merge({
+                    //             right: {
+                    //                 load_port_name: port("right")("port_name"),//r.row["right"]["port_name"]
+                    //                 load_port_code: port("right")("port_code")
+                    //             }
+                    //         })
+                    //     }).without({ right: ["id", "port_name", "port_code", "country_id"] }).zip()
+                    //     .eqJoin("dest_port_id", r.table("port")).map(function (port) {
+                    //         return port.merge({
+                    //             right: {
+                    //                 dest_port_name: port("right")("port_name"),//r.row["right"]["port_name"]
+                    //                 dest_port_code: port("right")("port_code")
+                    //             }
+                    //         })
+                    //     }).without({ right: ["id", "port_name", "port_code", "country_id"] }).zip()
+                    //     .eqJoin("deli_port_id", r.table("port")).map(function (port) {
+                    //         return port.merge({
+                    //             right: {
+                    //                 deli_port_name: port("right")("port_name"),//r.row["right"]["port_name"]
+                    //                 deli_port_code: port("right")("port_code")
+                    //             }
+                    //         })
+                    //     }).without({ right: ["id", "port_name", "port_code", "country_id"] }).zip()
+                    //     .eqJoin("cl_id", r.table("confirm_letter")).without({
+                    //         right: ["id", "cl_quality", "cl_type_rice", "cl_total_quantity", "cl_date"]
+                    //     }).zip()
+                    //     .eqJoin("carrier_id", r.table("carrier")).without({ right: "id" }).zip()
+                    //     .eqJoin("seller_id", r.db('external_f3').table("seller")).without({ right: ["id", "country_id"] }).zip()
+                    //     .eqJoin("ship_id", r.table("ship")).without({ right: "id" }).zip()
+                    //     .eqJoin("shipline_id", r.table("shipline")).without({ right: "id" }).zip()
+                    //     .eqJoin("surveyor_id", r.table("surveyor")).without({ right: "id" }).zip()
+                    //     .eqJoin("type_rice_id", r.table("type_rice")).without({ right: "id" }).zip()
+                    //     .eqJoin("package_id", r.table("package")).without({ right: "id" }).zip()
+                    //     // .eqJoin("country_id", r.table("country")).without({ right: "id" }).zip()
+                    //     .merge(function (r) {
+                    //         return { shm_det_id: r('id') }
+                    //     })
+                    //     .without('id')
+                    //     .coerceTo('array'),
+                    // shm_quantity: r.table("shipment_detail")
+                    //     .filter({ "shm_id": row('id') })
+                    //     .sum("shm_det_quantity")
                 }
             })
             .without('id')
