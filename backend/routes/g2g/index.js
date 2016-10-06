a954653efb345c4ac02364a490272fc11202b898
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-10-06 11:26:14 +0700

    remove  shipment detail

--- backend/routes/g2g/index.js
+++ backend/routes/g2g/index.js
@@ -4,10 +4,9 @@ var app = express();
 var contract = require('./contract');
 var confirmLetter = require('./confirmLetter');
 var shipment = require('./shipment');
-var shipmentDetail = require('./shipmentDetail');
+
 app.use('/contract', contract);
 app.use('/confirm', confirmLetter);
 app.use('/shipment', shipment);
-app.use('/shipment/detail', shipmentDetail);
 
 module.exports = app;
