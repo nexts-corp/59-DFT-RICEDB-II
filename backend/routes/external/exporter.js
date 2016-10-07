790a7d623175620d8937861194af31cad3dffe12
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-10-07 14:32:18 +0700

    split date time

--- backend/routes/external/exporter.js
+++ backend/routes/external/exporter.js
@@ -4,24 +4,33 @@ var router = express.Router();
 var r = require('rethinkdb');
 var db = require('../../db.js');
 
+var dd = new Date();
+var y = dd.getFullYear();
+var m = dd.getMonth();
+var d = dd.getDate();
+var d1y = (y - 1) + '-' + (m < 9 ? '0' : '') + (m + 1) + '-' + (d < 10 ? '0' : '') + d + "T00:00:00.000Z";
 
 router.get(['/', '/list'], function (req, res, next) {
-    var dd = new Date();
-    var y = dd.getFullYear();
-    var m = dd.getMonth();
-    var d = dd.getDate();
-    var d1y = (y - 1) + '-' + (m < 9 ? '0' : '') + (m + 1) + '-' + (d < 10 ? '0' : '') + d + "T00:00:00.000Z";
-    
     db.query(function (conn) {
         r.db('external_f3').table("exporter")
             .merge(function (row) {
                 return { 
                     exporter_id: row('id'),
-                    exporter_status:r.ISO8601(d1y).toEpochTime().lt(r.ISO8601(row('exporter_date_update')).toEpochTime()) 
+                    exporter_status:r.ISO8601(d1y).toEpochTime().lt(r.ISO8601(row('exporter_date_update')).toEpochTime()),
+                    exporter_date_approve:row('exporter_date_approve').split('T')(0),
+                    exporter_date_create:row('exporter_date_create').split('T')(0),
+                    exporter_date_update:row('exporter_date_update').split('T')(0) 
                 }
             })
             .without('id')
-            .eqJoin("trader_id", r.db('external_f3').table("trader")).without({ right: "id" }).zip()
+            .eqJoin("trader_id", r.db('external_f3').table("trader"))
+            .without({ right: "id" }).zip()
+            .merge(function(r){
+                return {
+                    trader_date_approve:r('trader_date_approve').split('T')(0),
+                    trader_date_expire:r('trader_date_expire').split('T')(0)
+                }
+            })
             .eqJoin("seller_id", r.db('external_f3').table("seller")).without({ right: "id" }).zip()
             .eqJoin("type_lic_id", r.db('external_f3').table("type_license")).without({ right: "id" }).zip()
             .eqJoin("country_id", r.table("country")).without({ right: "id" }).zip()
@@ -45,9 +54,20 @@ router.get('/:exporter_id', function (req, res, next) {
     db.query(function (conn) {
         r.db('external_f3').table("exporter")
             .get(req.params.exporter_id)
-            .merge(
-            { exporter_id: r.row('id') },
-            r.db('external_f3').table("trader").get(r.row("trader_id")),
+            .merge({ 
+                exporter_id: r.row('id'),
+                exporter_status:r.ISO8601(d1y).toEpochTime().lt(r.ISO8601(r.row('exporter_date_update')).toEpochTime()),
+                exporter_date_approve:r.row('exporter_date_approve').split('T')(0),
+                exporter_date_create:r.row('exporter_date_create').split('T')(0),
+                exporter_date_update:r.row('exporter_date_update').split('T')(0)  
+            },
+            r.db('external_f3').table("trader").get(r.row("trader_id"))
+            .merge(function(r){
+                return {
+                    trader_date_approve:r('trader_date_approve').split('T')(0),
+                    trader_date_expire:r('trader_date_expire').split('T')(0)
+                }
+            }),
             r.db('external_f3').table("seller").get(r.row("seller_id")),
             r.db('external_f3').table("type_license").get(r.row("type_lic_id")),
             r.table("country").get(r.row("country_id"))
