726df3320397e96c7aaf079660b89f2971afe883
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-10-10 14:38:56 +0700

    edit shipment detail

---
+++ backend/routes/g2g/billLoad.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+
+var r = require('rethinkdb');
+var db = require('../../db.js');
+
+var Ajv = require('ajv');
+var ajv = Ajv({ allErrors: true });
+var schema = {
+    //'type': 'object',
+    "properties": {
+        "id": {
+            "type": "string"
+        },
+        "contract_name": {
+            "type": "string"
+        },
+        "buyer_id": {
+            "type": "string"
+        },
+        "contract_date": {
+            "type": "string",
+            "format": "date"
+        },
+        "contract_desc": {
+            "type": "string"
+        },
+        "contract_type_rice": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "type_rice_id": { "type": "string" },
+                    "type_rice_quantity": { "type": "number" },
+                },
+                "required": ["type_rice_id", "type_rice_quantity"]
+            }
+        }
+    },
+    "required": ["contract_name", "buyer_id", "contract_date", "contract_type_rice"]
+};
+var validate = ajv.compile(schema);
+
+router.get(['/', '/list'], function (req, res, next) {
+    db.query(function (conn) {
+        r.table('shipment_detail')
+            .group(r.row('bl_no'), {multi: true})
+            // .concatMap(function (r) {
+            //     return r('shipment_detail')('bl_no')
+            // })
+            .distinct()
+            .merge(function(r){
+                return {
+                    
+                }
+            })
+            .run(conn, function (err, cursor) {
+                if (!err) {
+                    cursor.toArray(function (err, result) {
+                        if (!err) {
+                            //console.log(JSON.stringify(result, null, 2));
+                            res.json(result);
+                        } else {
+                            res.json(null);
+                        }
+                    });
+                } else {
+                    res.json(null);
+                }
+            });
+    })
+});
+
+
+module.exports = router;
--- backend/routes/g2g/index.js
+++ backend/routes/g2g/index.js
@@ -4,9 +4,13 @@ var app = express();
 var contract = require('./contract');
 var confirmLetter = require('./confirmLetter');
 var shipment = require('./shipment');
+var shipmentDetail = require('./shipmentDetail');
+var billLoad = require('./billLoad');
 
 app.use('/contract', contract);
 app.use('/confirm', confirmLetter);
 app.use('/shipment', shipment);
+app.use('/shipment/detail', shipmentDetail);
+app.use('/bl', billLoad);
 
 module.exports = app;
