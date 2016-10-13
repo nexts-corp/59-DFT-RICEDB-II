7a004b09e7bfff5c238243caaffbd60f789af94a
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-10-13 18:01:27 +0700

    shipment test insert

--- backend/routes/g2g/shipment.js
+++ backend/routes/g2g/shipment.js
@@ -160,41 +160,42 @@ router.get('/id/:shm_id', function (req, res, next) {
 });
 router.post('/insert', function (req, res, next) {
     //console.log(req.body);
-    var valid = validate(req.body);
-    var result = { result: false, message: null, id: null };
-    if (valid) {
-        //console.log(req.body);
-        res.json(req.body);
-        // if (req.body.id == null) {
-        //     //result.id = req.body.id;
-        //     db.query(function (conn) {
-        //         r.table("shipment")
-        //             //.get(req.body.id)
-        //             .insert(req.body)
-        //             .run(conn)
-        //             .then(function (response) {
-        //                 result.message = response;
-        //                 if (response.errors == 0) {
-        //                     result.result = true;
-        //                     result.id = response.generated_keys;
-        //                 }
-        //                 res.json(result);
-        //                 console.log(result);
-        //             })
-        //             .error(function (err) {
-        //                 result.message = err;
-        //                 res.json(result);
-        //                 console.log(result);
-        //             })
-        //     })
-        // } else {
-        //     result.message = 'field "id" must do not have data';
-        //     res.json(result);
-        // }
-    } else {
-        result.message = ajv.errorsText(validate.errors);
-        res.json(result);
-    }
+    res.json(req.body);
+    // var valid = validate(req.body);
+    // var result = { result: false, message: null, id: null };
+    // if (valid) {
+    //     //console.log(req.body);
+    //     res.json(req.body);
+    //     // if (req.body.id == null) {
+    //     //     //result.id = req.body.id;
+    //     //     db.query(function (conn) {
+    //     //         r.table("shipment")
+    //     //             //.get(req.body.id)
+    //     //             .insert(req.body)
+    //     //             .run(conn)
+    //     //             .then(function (response) {
+    //     //                 result.message = response;
+    //     //                 if (response.errors == 0) {
+    //     //                     result.result = true;
+    //     //                     result.id = response.generated_keys;
+    //     //                 }
+    //     //                 res.json(result);
+    //     //                 console.log(result);
+    //     //             })
+    //     //             .error(function (err) {
+    //     //                 result.message = err;
+    //     //                 res.json(result);
+    //     //                 console.log(result);
+    //     //             })
+    //     //     })
+    //     // } else {
+    //     //     result.message = 'field "id" must do not have data';
+    //     //     res.json(result);
+    //     // }
+    // } else {
+    //     result.message = ajv.errorsText(validate.errors);
+    //     res.json(result);
+    // }
 });
 router.put('/update', function (req, res, next) {
     //console.log(req.body);
