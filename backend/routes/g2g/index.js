02969c9732a618899e0412f5bebb4e244bc98df4
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-10-19 20:43:23 +0700

    add invoice

--- backend/routes/g2g/index.js
+++ backend/routes/g2g/index.js
@@ -6,11 +6,13 @@ var confirmLetter = require('./confirmLetter');
 var shipment = require('./shipment');
 var shipmentDetail = require('./shipmentDetail');
 var billLoad = require('./billLoad');
+var invoice = require('./invoice');
 
 app.use('/contract', contract);
 app.use('/confirm', confirmLetter);
 app.use('/shipment', shipment);
 app.use('/shipment/detail', shipmentDetail);
 app.use('/bl', billLoad);
+app.use('/invoice', invoice);
 
 module.exports = app;
