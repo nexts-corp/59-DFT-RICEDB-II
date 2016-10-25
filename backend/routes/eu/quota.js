af24bd2ae31301e7434d7636bb71635a09303e16
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2016-10-25 18:33:38 +0700

    new query eu

--- backend/routes/eu/quota.js
+++ backend/routes/eu/quota.js
@@ -10,26 +10,32 @@ var ajv = Ajv({ allErrors: true, coerceTypes: 'array' });
 router.get(['/'], function (req, res, next) {
     db.query(function (conn) {
             
-            statemant = r.db('eu').table('quota');
-
             var params = req.query;
-            if(typeof params != "undefined"){
-                for(var key in params){
-                    if(key=="id")
-                    params[key] = parseInt(params[key]);
-                }
-                statemant = statemant.filter(params);
-            }
+            var statemant = r.db('eu').table('quota').get(params.id).merge(function(row){
+                return {type_rice:row('type_rice').merge(function(type_rice){
+                    return (r.db('eu').table('type_rice').get(type_rice('type_rice_id')).without('id'))
+                })}
+            })
+
+            // if(typeof params != "undefined"){
+            //     for(var key in params){
+            //         if(key=="id")
+            //         params[key] = parseInt(params[key]);
+            //     }
+            //     statemant = statemant.filter(params);
+            // }
             
             statemant.run(conn, function (err, cursor) {
                 if (!err) {
-                    cursor.toArray(function (err, result) {
-                        if (!err) {
-                            res.json(result);
-                        } else {
-                            res.json(null);
-                        }
-                    });
+                    console.log(cursor);
+                    res.json(cursor);
+                    // cursor.toArray(function (err, result) {
+                    //     if (!err) {
+                    //         res.json(result);
+                    //     } else {
+                    //         res.json(null);
+                    //     }
+                    // });
                 } else {
                     res.json(null);
                 }
@@ -39,108 +45,114 @@ router.get(['/'], function (req, res, next) {
 
 
 
-var schema = {
+// var schema = {
 
-    "properties": {
-        "id": {
-            "type": "number"
-        },
-        "period": {
-            "type": "array",
-            "properties":{
-                "percent":{
-                    "type": "number"
-                },
-                "quantity":{
-                    "type": "number"
-                }
-            },
-            "required": [
-                "percent",
-                "quantity"
-            ]
-        },
-        "type_rice_id": {
-            "type": "string"
-        }
-    },
-    "required": [
-        "id",
-        "period",
-        "type_rice_id"
-    ]
-};
+//     "properties": {
+//         "id": {
+//             "type": "number"
+//         },
+//         "period": {
+//             "type": "array",
+//             "properties":{
+//                 "percent":{
+//                     "type": "number"
+//                 },
+//                 "quantity":{
+//                     "type": "number"
+//                 }
+//             },
+//             "required": [
+//                 "percent",
+//                 "quantity"
+//             ]
+//         },
+//         "type_rice_id": {
+//             "type": "string"
+//         }
+//     },
+//     "required": [
+//         "id",
+//         "period",
+//         "type_rice_id"
+//     ]
+// };
 
-var validate = ajv.compile(schema);
+// var validate = ajv.compile(schema);
 
 
-router.post(['/'], function (req, res, next) {
+// router.post(['/'], function (req, res, next) {
+
+//     //if(r.db('eu').table('quota').get(req.body.id))
+
+//     r.do(r.db('eu').table('quota').get(req.body.id),function(x){
+//         console.log('1');
+//     });
     
-    var valid = validate(req.body);
+//     // var valid = validate(req.body);
     
-    if(valid){
-        console.log(req.body);
-        db.query(function (conn) {
+//     // if(valid){
+//     //     console.log(req.body);
+//     //     db.query(function (conn) {
 
-            r.db('eu').table('quota').insert(req.body)
-            .run(conn, function (err, cursor) {
-                if (!err) {
-                    res.json(cursor);
-                } else {
-                    res.json(null);
-                }
-            });
+//     //         r.db('eu').table('quota').insert(req.body)
+//     //         .run(conn, function (err, cursor) {
+//     //             if (!err) {
+//     //                 res.json(cursor);
+//     //             } else {
+//     //                 res.json(null);
+//     //             }
+//     //         });
 
-        });
-    }else{
-        res.json({error:"schema"}); 
-    }
-
-    
-});
-
-router.put(['/'], function (req, res, next) {
-    
-    var valid = validate(req.body);
-
-    if(valid){
-        db.query(function (conn) {
-
-            r.db('eu').table('quota').get(req.body.id).update(req.body)
-            .run(conn, function (err, cursor) {
-                if (!err) {
-                    console.log(req.body);
-                    res.json(req.body);
-                } else {
-                    res.json(null);
-                }
-            });
-
-        });
-    }else{
-        res.json({error:"schema"}); 
-    }
+//     //     });
+//     // }else{
+//     //     res.json({error:"schema"}); 
+//     // }
 
     
-});
+// });
+
+// router.put(['/'], function (req, res, next) {
+    
+//     var valid = validate(req.body);
+
+//     if(valid){
+//         db.query(function (conn) {
+
+//             r.db('eu').table('quota').get(req.body.id).update(req.body)
+//             .run(conn, function (err, cursor) {
+//                 if (!err) {
+//                     console.log(req.body);
+//                     res.json(req.body);
+//                 } else {
+//                     res.json(null);
+//                 }
+//             });
+
+//         });
+//     }else{
+//         res.json({error:"schema"}); 
+//     }
+
+    
+// });
 
 
-router.delete(['/'], function (req, res, next) {
+// router.delete(['/'], function (req, res, next) {
   
-        db.query(function (conn) {
+//         db.query(function (conn) {
 
-            r.db('eu').table('quota').get(parseInt(req.query.id)).delete()
-            .run(conn, function (err, cursor) {
-                if (!err) {
-                    res.json(cursor);
-                } else {
-                    res.json(null);
-                }
-            });
+//             r.db('eu').table('quota').get(parseInt(req.query.id)).delete()
+//             .run(conn, function (err, cursor) {
+//                 if (!err) {
+//                     res.json(cursor);
+//                 } else {
+//                     res.json(null);
+//                 }
+//             });
 
-        });
+//         });
     
-});
+// });
 
 module.exports = router;
 
