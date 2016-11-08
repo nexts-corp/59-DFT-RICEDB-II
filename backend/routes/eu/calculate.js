50978977b03bbca30cef441883de5dd470999fd9
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2016-11-08 15:48:48 +0700

    eu get allocate_quota

--- backend/routes/eu/calculate.js
+++ backend/routes/eu/calculate.js
@@ -9,10 +9,10 @@ var ajv = Ajv({ allErrors: true, coerceTypes: 'array' });
 
 
 
-router.get(['/year'], function (req, res, next) {
-    db.query(function (conn) {
+router.get(['/year'], function(req, res, next) {
+    db.query(function(conn) {
         var statement = r.db('eu').table('ex_export_quantity_eu').pluck('year').orderBy(r.desc('year'))('year').distinct();
-        statement.run(conn, function (err, cursor) {
+        statement.run(conn, function(err, cursor) {
             if (!err) {
                 res.json(cursor);
             } else {
@@ -22,27 +22,27 @@ router.get(['/year'], function (req, res, next) {
     });
 });
 
-router.get(['/exporter'], function (req, res, next) {
+router.get(['/exporter'], function(req, res, next) {
     var params = req.query;
-    db.query(function (conn) {
+    db.query(function(conn) {
         var statement = r.db('eu').table('ex_export_quantity_eu')
             .filter(
-            function (row) {
+            function(row) {
                 return row('type_rice_id').eq(params.type_rice_id)
                     .and(row("year").gt(String(parseInt(params.frist_year) - 1)).and(row("year").lt(String(parseInt(params.last_year) + 1))))
             }
-            )("exporter_id").distinct().map(function (row) {
+            )("exporter_id").distinct().map(function(row) {
                 return { id: row }
-            }).merge(function (row) {
+            }).merge(function(row) {
                 return r.db('external_f3').table('exporter').filter({ id: row('id') })
-                    .merge(function (exporter) { return { exporter_id: exporter('id') } })
+                    .merge(function(exporter) { return { exporter_id: exporter('id') } })
                     .eqJoin('trader_id', r.db('external_f3').table('trader')).zip()
                     .eqJoin('seller_id', r.db('external_f3').table('seller')).zip()
-                    .merge(function (seller) { return { id: seller('exporter_id') } }).pluck('id', 'seller_name_th')(0)
+                    .merge(function(seller) { return { id: seller('exporter_id') } }).pluck('id', 'seller_name_th')(0)
 
             });
 
-        statement.run(conn, function (err, cursor) {
+        statement.run(conn, function(err, cursor) {
             if (!err) {
                 res.json(cursor);
             } else {
@@ -52,20 +52,20 @@ router.get(['/exporter'], function (req, res, next) {
     });
 });
 
-router.post(['/calculate'], function (req, res, next) {
+router.post(['/calculate'], function(req, res, next) {
     var params = req.body;
-    db.query(function (conn) {
+    db.query(function(conn) {
 
         statement = r.do(
 
             r.db('eu').table('ex_export_quantity_eu')
                 .filter(
-                function (row) {
+                function(row) {
                     return row('type_rice_id').eq(params.type_rice_id)
                         .and(row("year").gt(String(parseInt(params.frist_year) - 1)).and(row("year").lt(String(parseInt(params.last_year) + 1))))
                 }
                 )
-                .filter(function (row) {
+                .filter(function(row) {
                     return r.expr(params.exporter_id).contains(row('exporter_id'))
                 })
                 .group('exporter_id')
@@ -74,18 +74,18 @@ router.post(['/calculate'], function (req, res, next) {
             ,
 
 
-            r.db('eu').table('confirm_quota').merge(function (row) {
+            r.db('eu').table('confirm_quota').merge(function(row) {
                 return {
-                    quantity: row('quantity')('quantity').reduce(function (left, right) {
+                    quantity: row('quantity')('quantity').reduce(function(left, right) {
                         return left.add(right)
                     }), year: row('quota_id')
                 }
             }).filter(
-                function (row) {
+                function(row) {
                     return row('type_rice_id').eq(params.type_rice_id)
                         .and(row("year").gt(String(parseInt(params.frist_year) - 1)).and(row("year").lt(String(parseInt(params.last_year) + 1))))
                 }
-                ).filter(function (row) {
+                ).filter(function(row) {
                     return r.expr(params.exporter_id).contains(row('exporter_id'))
                 })
                 .group('exporter_id').sum('quantity').ungroup()
@@ -93,55 +93,55 @@ router.post(['/calculate'], function (req, res, next) {
             ,
 
 
-            function (sum_export, sum_quota) {
+            function(sum_export, sum_quota) {
                 return r.do(
 
                     sum_export.union(sum_quota)
-                        .group('group').map(function (row) { return row('reduction') }).reduce(function (left, right) { return left.sub(right) }).ungroup()
-                        .map(function (row) {
+                        .group('group').map(function(row) { return row('reduction') }).reduce(function(left, right) { return left.sub(right) }).ungroup()
+                        .map(function(row) {
                             return {
                                 quantity: r.branch(row('reduction').lt(0), 0, row('reduction')),
                                 exporter_id: row('group')
                             }
                         })
-                        .filter(function (row) {
+                        .filter(function(row) {
                             return row('quantity').eq(0).not()
                         })
 
                     ,
 
                     r.db('eu').table('quota').get(params.year_quota).merge(
-                        function (row) {
+                        function(row) {
                             return row('type_rice')
                         }
                     ).filter({ type_rice_id: params.type_rice_id })
 
-                    , function (result1, result2) {
+                    , function(result1, result2) {
 
 
-                        return result1.merge(function (row) {
+                        return result1.merge(function(row) {
                             return { quantity: row('quantity').mul(result2('amount')(0)).div(result1.sum('quantity')) }
-                        }).merge(function (row) {
+                        }).merge(function(row) {
                             return { period: result2('period')(0) }
-                        }).merge(function (row) {
+                        }).merge(function(row) {
                             return {
-                                period: row('period').merge(function (period) {
+                                period: row('period').merge(function(period) {
                                     return {
-                                        quantity: row('quantity').mul(period('percent')).div(100).do(function (result) {
+                                        quantity: row('quantity').mul(period('percent')).div(100).do(function(result) {
                                             return r.branch(result.lt(0), 0, result)
                                         })
                                     }
                                 })
-                                    .merge(function (row) {
+                                    .merge(function(row) {
                                         return { quantity_update: row('quantity') }
                                     })
                             }
-                        }).do(function (exporter) {
+                        }).do(function(exporter) {
                             return {
                                 sum_for_cal: result1.sum('quantity'),
                                 sum_export: sum_export.sum('reduction'),
                                 sum_quota: sum_quota.sum('reduction'),
-                                quota_amount:result2(0)('amount'),
+                                quota_amount: result2(0)('amount'),
                                 exporter: exporter
                             }
                         })
@@ -153,7 +153,7 @@ router.post(['/calculate'], function (req, res, next) {
 
         )
 
-        statement.run(conn, function (err, cursor) {
+        statement.run(conn, function(err, cursor) {
             if (!err) {
                 res.json(cursor);
             } else {
@@ -167,8 +167,47 @@ router.post(['/calculate'], function (req, res, next) {
 });
 
 
-router.post(['/allocate_quota'], function (req, res, next) {
-    
+router.get(['/allocate_quota'], function(req, res, next) {
+
+    var params = req.query;
+    db.query(function(conn) {
+        statement = r.db('eu').table('allocate_quota').without('exporter')
+            .filter({ quota_id: params.quota_id }).group('type_rice_id')
+            .ungroup()
+            .map(function(row) {
+                return {
+                    type_rice_id: row('group'),
+                    detail: row('reduction')
+                }
+            })
+            .eqJoin('type_rice_id', r.db('eu').table('type_rice')).zip().without('id');
+        statement.run(conn, function(err, cursor) {
+            if (!err) {
+                res.json(cursor);
+            } else {
+                res.json({ error: "error" });
+            }
+        });
+    });
+
+
+});
+
+router.post(['/allocate_quota'], function(req, res, next) {
+
+    var params = req.body;
+    db.query(function(conn) {
+        statement = r.db('eu').table('allocate_quota').insert(params);
+        statement.run(conn, function(err, cursor) {
+            if (!err) {
+                res.json(cursor);
+            } else {
+                res.json({ error: "error" });
+            }
+        });
+    });
+
+
 });
 
 module.exports = router;
