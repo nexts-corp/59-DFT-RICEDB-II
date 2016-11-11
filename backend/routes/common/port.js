be8df696233766aa16b9b8a1472030f7df080569
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-11-11 11:57:55 +0700

    CRUD common

--- backend/routes/common/port.js
+++ backend/routes/common/port.js
@@ -48,7 +48,103 @@ router.get('/id/:port_id', function (req, res, next) {
             });
     })
 });
-
+router.post('/insert', function (req, res, next) {
+    var valid = validate(req.body);
+    var result = { result: false, message: null, id: null };
+    if (valid) {
+        if (req.body.id == null) {
+            db.query(function (conn) {
+                r.db('common').table("port")
+                    .insert(req.body)
+                    .run(conn)
+                    .then(function (response) {
+                        result.message = response;
+                        if (response.errors == 0) {
+                            result.result = true;
+                            result.id = response.generated_keys;
+                        }
+                        res.json(result);
+                        console.log(result);
+                    })
+                    .error(function (err) {
+                        result.message = err;
+                        res.json(result);
+                        console.log(result);
+                    })
+            })
+        } else {
+            result.message = 'field "id" must do not have data';
+            res.json(result);
+        }
+    } else {
+        result.message = ajv.errorsText(validate.errors);
+        res.json(result);
+    }
+});
+router.put('/update', function (req, res, next) {
+    //console.log(req.body);
+    var valid = validate(req.body);
+    var result = { result: false, message: null, id: null };
+    if (valid) {
+        //console.log(req.body);
+        if (req.body.id != '' && req.body.id != null) {
+            result.id = req.body.id;
+            db.query(function (conn) {
+                r.db('common').table("port")
+                    .get(req.body.id)
+                    .update(req.body)
+                    .run(conn)
+                    .then(function (response) {
+                        result.message = response;
+                        if (response.errors == 0) {
+                            result.result = true;
+                        }
+                        res.json(result);
+                        console.log(result);
+                    })
+                    .error(function (err) {
+                        result.message = err;
+                        res.json(result);
+                        console.log(result);
+                    })
+            })
+        } else {
+            result.message = 'require field id';
+            res.json(result);
+        }
+    } else {
+        result.message = ajv.errorsText(validate.errors);
+        res.json(result);
+    }
+});
+router.delete('/delete/id/:id', function (req, res, next) {
+    var result = { result: false, message: null, id: null };
+    if (req.params.id != '' || req.params.id != null) {
+        result.id = req.params.id;
+        db.query(function (conn) {
+            r.db('common').table("port")
+                .get(req.params.id)
+                .delete()
+                .run(conn)
+                .then(function (response) {
+                    result.message = response;
+                    if (response.errors == 0) {
+                        result.result = true;
+                    }
+                    res.json(result);
+                    console.log(result);
+                })
+                .error(function (err) {
+                    result.message = err;
+                    res.json(result);
+                    console.log(result);
+                })
+        })
+    } else {
+        result.message = 'require field id';
+        res.json(result);
+    }
+});
 
 module.exports = router;
 
