a8095184ceb9959ceead68c1a65b8bd725cc8ab8
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2016-11-23 13:53:15 +0700

    add worker

--- a-backend/index.js
+++ a-backend/index.js
@@ -3,33 +3,58 @@ var public = express();
 var bodyParser = require('body-parser');
 var app = express();
 
-var test = require('./routes/test');
-var g2g = require('./routes/g2g');
-var common = require('./routes/common');
-var ext_f3 = require('./routes/external');
-var eu = require('./routes/eu');
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-// parse application/json
-app.use(bodyParser.json())
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`[worker] ${worker.process.pid} died (T_T)`);
+    cluster.fork();
+  });
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect 
-  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-  // res.setHeader('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
-  return next();
-});
-app.use(test);
-app.use('/g2g', g2g);
-app.use('/common', common);
-app.use('/external', ext_f3);
-app.use('/eu', eu);
+  cluster.on('listening',(worker) => {
+    console.log(`[worker] ${worker.process.pid} start (^.^)`);
+  });
+} else {
+
+　
+  var test = require('./routes/test');
+  var g2g = require('./routes/g2g');
+  var common = require('./routes/common');
+  var ext_f3 = require('./routes/external');
+  var eu = require('./routes/eu');
+
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }))
+
+  // parse application/json
+  app.use(bodyParser.json())
+
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect 
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    // res.setHeader('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
+    return next();
+  });
+  app.use(test);
+  app.use('/g2g', g2g);
+  app.use('/common', common);
+  app.use('/external', ext_f3);
+  app.use('/eu', eu);
 
 
-public.use('/api', app);
-public.listen(3000);
+  public.use('/api', app);
+  public.listen(3000);
+
+
+
+}
+
