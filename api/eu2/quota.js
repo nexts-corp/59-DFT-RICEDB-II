f38b2e6699c992bbb6bba70685daa7e021c9dc9c
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2016-12-08 11:59:34 +0700

    try mqtt

--- api/eu2/quota.js
+++ api/eu2/quota.js
@@ -7,14 +7,45 @@ var db = require('../../db.js');
 var mqtt = require('mqtt');
 var client  = mqtt.connect('mqtt://mqtt.codeunbug.com');
 
+const _ = require('lodash');
+
 client.on('connect', function () {
-  client.subscribe('testxx');
+  client.subscribe('eu-quotaPut-'+global.mqttId);
 })
 
 client.on('message', function (topic, message) {
   
-  console.log(topic,message.toString());
-  client.publish('testzz','123456');
+  if(topic=="eu-quotaPut-"+global.mqttId){
+    
+    db.query(function (conn) {
+      var data = JSON.parse(message.toString());
+
+      var statement = r.db('eu2').table('quota').filter({
+        type_rice_id:data.type_rice_id,
+        id:data.year
+      }).coerceTo('array').do(function(result){
+        return r.branch(
+          result.count().eq(0),
+          r.db('eu2').table('quota').insert(r.expr(data).merge(function(row){
+            return {id:row('year')}
+          }).without('year')),
+          r.db('eu2').table('quota').get(result(0)('id')).update(r.expr(data).without('year','id'))
+        )
+      })
+
+      ;
+
+      statement.run(conn, function (err, cursor) {
+          if (!err) {
+            console.log(cursor);
+            client.publish('eu-quota-updated',JSON.stringify(cursor));
+          } else {
+            console.log(err);
+          }
+      });
+    });
+  }
+  //client.publish('testzz','123456');
   //setInterval(()=>{ client.publish('testzz','testzz'); }, 1000);
   //client.end();
   
