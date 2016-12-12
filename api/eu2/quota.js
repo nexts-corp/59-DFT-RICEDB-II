1c99aa3ef0a04ec76430acfbce16851ae252303b
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2016-12-12 19:42:31 +0700

    ok test

--- api/eu2/quota.js
+++ api/eu2/quota.js
@@ -9,58 +9,36 @@ var client  = mqtt.connect('mqtt://mqtt.codeunbug.com');
 
 const _ = require('lodash');
 
-client.on('connect', function () {
-  client.subscribe('eu-quotaPut-'+global.mqttId);
-  client.subscribe('eu/quota/req');
 
-  setTimeout(function(){
-    client.publish('eu/quota','data form server');
-  },3000);
-})
 
-client.on('message', function (topic, message, data) {
-  
-  if(topic=="eu-quotaPut-"+global.mqttId){
-    
-    db.query(function (conn) {
-      var data = JSON.parse(message.toString());
 
-      var statement = r.db('eu2').table('quota').filter({
-        type_rice_id:data.type_rice_id,
-        id:data.year
-      }).coerceTo('array').do(function(result){
-        return r.branch(
-          result.count().eq(0),
-          r.db('eu2').table('quota').insert(r.expr(data).merge(function(row){
-            return {id:row('year')}
-          }).without('year')),
-          r.db('eu2').table('quota').get(result(0)('id')).update(r.expr(data).without('year','id'))
-        )
-      })
+// db.query(function (conn) {
+//       var data = JSON.parse(message.toString());
 
-      ;
+//       var statement = r.db('eu2').table('quota').filter({
+//         type_rice_id:data.type_rice_id,
+//         id:data.year
+//       }).coerceTo('array').do(function(result){
+//         return r.branch(
+//           result.count().eq(0),
+//           r.db('eu2').table('quota').insert(r.expr(data).merge(function(row){
+//             return {id:row('year')}
+//           }).without('year')),
+//           r.db('eu2').table('quota').get(result(0)('id')).update(r.expr(data).without('year','id'))
+//         )
+//       })
 
-      statement.run(conn, function (err, cursor) {
-          if (!err) {
-            console.log(cursor);
-            client.publish('eu-quota-updated',JSON.stringify(cursor));
-            // setInterval(function(){
-            //   client.publish('eu-quota-updated',JSON.stringify(cursor));
-            // },1000);
-            
-          } else {
-            console.log(err);
-          }
-      });
-    });
-  }else if(topic='eu/quota/req'){
-    console.log(data);
-  }
-  //client.publish('testzz','123456');
-  //setInterval(()=>{ client.publish('testzz','testzz'); }, 1000);
-  //client.end();
-  
-})
+//       ;
+
+//       statement.run(conn, function (err, cursor) {
+//           if (!err) {
+//             console.log(cursor);
+//             client.publish('eu-quota-updated',JSON.stringify(cursor));        
+//           } else {
+//             console.log(err);
+//           }
+//       });
+// });
 
 module.exports = router;
 
