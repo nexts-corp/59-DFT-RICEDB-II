8fc0e11326b66734ff91383e78ae27189e97f273
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-12-15 11:08:50 +0700

    check duplicate get

--- backend/routes/common/check.js
+++ backend/routes/common/check.js
@@ -4,10 +4,10 @@ var router = express.Router();
 var r = require('rethinkdb');
 var db = require('../../db.js');
 
-router.post('/duplicate', function (req, res, next) {
+router.get('/duplicate', function (req, res, next) {
     var q = {};
-    var tb = req.body['table'];
-    q[req.body['field']] = req.body['value'];
+    var tb = req.query['table'];
+    q[req.query['field']] = req.query['value'];
     db.query(function (conn) {
         r.db('common').table(tb)
             .filter(q)
