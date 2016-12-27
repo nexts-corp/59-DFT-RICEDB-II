a06b919bd725538f9f38b28b0867895d7edd836a
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2016-12-27 19:30:57 +0700

    edit quota

--- app/controllers/quota/index.controller.js
+++ app/controllers/quota/index.controller.js
@@ -1,10 +1,25 @@
 class index{
 
-    readQuota(req,res){
+    getQuota(req,res){
         var r = req._r;
         var params = req.query;
 
-        r.db('eu2').table('quota').coerceTo('array').run().then(function(result) {
+        // r.db('eu2').table('quota').filter({year:2559}).without('year')
+        // .eqJoin(r.row('type_rice_id'),r.db('eu2').table('type_rice')).coerceTo('array')
+
+
+        r.expr({year:2556}).merge(function(row){
+            return {
+                type_rice:
+                r.db('eu2').table('quota').filter({year:2559}).without('year')
+                .merge(function(row){
+                    return {quota_id:row('id')}
+                })
+                .eqJoin(function(row){return row('type_rice_id')},r.db('eu2').table('type_rice')).zip().without('id')
+                .coerceTo('array')
+            }
+        })
+        .run().then(function(result) {
             res.json(result);
         });
     }
--- app/routes/quota.routes.js
+++ app/routes/quota.routes.js
@@ -2,7 +2,7 @@ module.exports=function(app){
 
     var quotaIndexCtrl = require('../controllers/quota/index.controller');
 
-    app.get('/quota',quotaIndexCtrl.readQuota);
+    app.get('/quota',quotaIndexCtrl.getQuota);
     app.post('/quota',quotaIndexCtrl.insertQuota);
     app.put('/quota',quotaIndexCtrl.updateQuota);
     app.delete('/quota',quotaIndexCtrl.deleteQuota);
