015b058da734221d61c276755bb7f2fbd46c6505
chonlatee <chonlatee.s@nexts-corp.com>
2016-12-28 14:32:22 +0700

    edit insert

--- app/controllers/report/index.controller.js
+++ app/controllers/report/index.controller.js
@@ -10,16 +10,16 @@ class index{
         });
     }
     
-    // deleteReport(req,res){
-    //     var r = req._r;
-    //     console.log(req.params.id);
-    //     if(typeof req.params.id !== "undefined"){
-    //         req.query.id = parseInt(req.query.id);
-    //     }
-    //     r.db('eu2').table('report').delte(params).run().then(function(result){
-    //         res.json(result);
-    //     });
-    // }
+    deleteReport(req,res){
+        var r = req._r;
+        console.log(req.params.id);
+        // if(typeof req.params.id !== "undefined"){
+        //     req.params.id = parseInt(req.params.id);
+        // }
+        r.db('eu2').table('report').get(req.params.id).delete().run().then(function(result){
+            res.json(result);
+        });
+    }
 }
 
 module.exports = new index();
--- app/routes/report.routes.js
+++ app/routes/report.routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app){
 var reportIndexCtrl = require('../controllers/report/index.controller')
     app.post('/report',reportIndexCtrl.insertReport);
-    //app.delete('/report/:id',reportIndexCtrl.deleteReport);
+    app.delete('/report/:id',reportIndexCtrl.deleteReport);
 }
