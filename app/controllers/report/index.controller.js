572650578c3dcb2b8baf11679de416a2b93e5bbb
chonlatee <chonlatee.s@nexts-corp.com>
2016-12-28 15:06:26 +0700

    edit select

--- app/controllers/report/index.controller.js
+++ app/controllers/report/index.controller.js
@@ -20,6 +20,33 @@ class index{
             res.json(result);
         });
     }
+
+    selectReport(req,res){
+        var r = req._r;
+        var params = req.query;
+        
+        if(typeof params.quota !== "undefined"){
+            params.quota = Boolean(params.quota);
+            params.year = parseInt(params.year);
+            params.month = parseInt(params.month);
+        }
+        console.log(params)
+          r.db('eu2').table('report').filter({
+                year:params.year,
+                month:params.month,
+                quota:params.quota //ในโควต้า
+            }).without('id')
+            .innerJoin(r.db('eu2').table('exporter'), function(x,xx) {
+                return x('exporter_id') .eq(xx('id'))
+            }).zip()
+            .innerJoin(r.db('eu2').table('type_rice'), function(x,xx){
+                return x('type_rice_id') . eq(xx('id')) 
+            }).without('id').zip()
+            .run()
+            .then(function(result){
+                res.json(result);
+            });
+    }
 }
 
 module.exports = new index();
--- app/routes/report.routes.js
+++ app/routes/report.routes.js
@@ -1,5 +1,7 @@
 module.exports = function(app){
 var reportIndexCtrl = require('../controllers/report/index.controller')
-    app.post('/report',reportIndexCtrl.insertReport);
-    app.delete('/report/:id',reportIndexCtrl.deleteReport);
+    app.post('/report',reportIndexCtrl.insertReport); // post is insert
+    app.delete('/report/:id',reportIndexCtrl.deleteReport); // is delete
+    app.get('/report',reportIndexCtrl.selectReport);  //get is select
+    //app.put('/report',reportIndexCtrl.updateReport); // put is update
 }
