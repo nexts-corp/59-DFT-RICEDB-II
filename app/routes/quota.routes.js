889248b276a65cdf9dc3f5006acabd692e2c48b9
Kitz-Parnupat <dainokung2535@gmail.com>
2016-12-28 10:14:55 +0700

    api quota

--- app/routes/quota.routes.js
+++ app/routes/quota.routes.js
@@ -2,7 +2,7 @@ module.exports=function(app){
 
     var quotaIndexCtrl = require('../controllers/quota/index.controller');
 
-    app.get('/quota',quotaIndexCtrl.getQuota);
+    app.get('/quota/:year',quotaIndexCtrl.getQuota);
     app.get('/quota/:id',quotaIndexCtrl.selectQuota);
     app.post('/quota',quotaIndexCtrl.insertQuota);
     app.put('/quota',quotaIndexCtrl.updateQuota);
