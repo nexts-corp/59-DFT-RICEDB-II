f508aa284ab92928a8a7a1e6c576973ca394a945
chonlatee <chonlatee.s@nexts-corp.com>
2017-01-04 17:21:06 +0700

    select confirm all

--- app/controllers/confirm/index.controller.js
+++ app/controllers/confirm/index.controller.js
@@ -1,6 +1,6 @@
 class index{
     
-    selectconfirm(req,res){ // select for ordinal 
+    selectnotconfirm(req,res){ // select for ordinal 
         var r = req._r;
         var params = req.query;
         console.log('xxx7');
@@ -24,6 +24,42 @@ class index{
             res.json(result);
         });
     }
+
+    selectnotconfirm2(req,res){ // select all 
+        var r = req._r;
+        var params = req.query;
+
+        if(typeof params.year !== "undefined"){
+            params.year = parseInt(params.year);
+            params.ordinal = parseInt(params.ordinal);
+        }
+
+        r.db('eu2').table('quota').filter({
+            year: params.year,
+            type_rice_id: params.type_rice_id
+        })
+        .innerJoin(r.db('eu2').table('notify'), function(x,xx) {
+            return x('type_rice_id') .eq(xx('type_rice_id')).and( x('year') .eq(xx('year')))
+        }).map(function(join){
+            return join('right')
+            .merge(function(right){
+                return {
+                  quantity:right('quantity').merge(function(row){
+                  return {
+                  percent:join('left')('quantity').filter({period:row('period')})(0)('percent')
+                  }
+                })
+              }
+            })
+          }).filter({
+            exporter_id:params.exporter_id,
+            ordinal:params.ordinal
+          })
+        .run().then(function(result){
+            res.json(result);
+        });
+    }
+    
 }
 
 module.exports = new index();
--- app/routes/confirm.routes.js
+++ app/routes/confirm.routes.js
@@ -1,4 +1,5 @@
 module.exports = function(app){
 var confirmIndexCtrl = require('../controllers/confirm/index.controller')
-    app.get('/confirm',confirmIndexCtrl.selectconfirm);  //get is select
+    app.get('/confirm',confirmIndexCtrl.selectnotconfirm);  //get is select
+    app.get('/confirmall',confirmIndexCtrl.selectnotconfirm2);
 }
