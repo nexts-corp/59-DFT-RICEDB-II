f1d55ec7abcd897a11ecf2313a7cdff5c0aad012
chonlatee <chonlatee.s@nexts-corp.com>
2017-01-04 17:07:11 +0700

    add name and exporter_id

--- app/controllers/confirm/index.controller.js
+++ app/controllers/confirm/index.controller.js
@@ -1,6 +1,6 @@
 class index{
-
-    selectconfirm(req,res){
+    
+    selectconfirm(req,res){ // select for ordinal 
         var r = req._r;
         var params = req.query;
         console.log('xxx7');
@@ -14,8 +14,12 @@ class index{
             type_rice_id: params.type_rice_id
         })
         .innerJoin(r.db('eu2').table('notify'), function(x,xx) {
-            return x('type_rice_id') .eq(xx('type_rice_id')).and( x('year') .eq(xx('year')))
-        }).zip().pluck('ordinal')
+              return x('type_rice_id') .eq(xx('type_rice_id')).and( x('year') .eq(xx('year')))
+          }).zip()
+        .innerJoin(r.db('eu2').table('exporter'), function(i,ii) {
+              return i('exporter_id') .eq(ii('id'))
+            }).zip()  
+        .pluck('ordinal','exporter_id','name')
         .run().then(function(result){
             res.json(result);
         });
