e0bcf88ca3508248339d999bf660a319697f7a80
Muice^^ <moomookingza@gmail.com>
2017-01-06 14:27:41 +0700

    insert backofficeExternal

--- backend/routes/external/index.js
+++ backend/routes/external/index.js
@@ -5,9 +5,11 @@ var typeLicense = require('./typeLicense');
 var seller = require('./seller')
 var trader = require('./trader');
 var exporter = require('./exporter');
+var check = require('./check');
 
 app.use('/typeLicense', typeLicense);
 app.use('/seller', seller);
 app.use('/trader', trader);
 app.use('/exporter', exporter);
+app.use('/check', check);
 module.exports = app;
