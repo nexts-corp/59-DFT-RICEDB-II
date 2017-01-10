50b77760d9c708d79b6f7b3a19ef439f84cbfebb
chonlatee <chonlatee.s@nexts-corp.com>
2017-01-10 18:37:37 +0700

    update_sum

--- app/controllers/amount/index.controller.js
+++ app/controllers/amount/index.controller.js
@@ -8,35 +8,55 @@ class index{
             params.year = parseInt(params.year);
         }   
 
-        r.db('eu2').table('calculate').innerJoin(r.db('eu2').table('allocate'), function(c,a){
+      r.db('eu2').table('calculate').innerJoin(r.db('eu2').table('allocate'), function(c,a){
         return c('id').eq(a('calculate_id'))
-        }).map(function(mr){
-            return mr('right').merge(function(qu){
-            return {ordinal:mr('left')('ordinal')}
-            })
+      }).map(function(mr){
+        return mr('right').merge(function(qu){
+          return {ordinal:mr('left')('ordinal')}
         })
-            
+      })
+        
         .innerJoin(r.db('eu2').table('exporter'), function(all,e){
         return all('exporter_id').eq(e('id'))
-        }).map(function(ml){
-            return ml('left').merge(function(mn){
-            return { name:ml('right')('name') }
-            })
+      }).map(function(ml){
+        return ml('left').merge(function(mn){
+           return { name:ml('right')('name') }
         })
+      })
         
-        .innerJoin(r.db('eu2').table('quota'), function(ta,tq){
+      .innerJoin(r.db('eu2').table('quota'), function(ta,tq){
         return ta('quota_id').eq(tq('id'))
-        }).map(function(ma){
-            return ma('left').merge(function(x){
-            return {year:ma('right')('year'), type_rice_id:ma('right')('type_rice_id')}
+      }).map(function(ma){
+        return ma('left').merge(function(x){
+          return {
+            year:ma('right')('year'), 
+            type_rice_id:ma('right')('type_rice_id'),
+            quantity:ma('left')('quantity').merge(function(rowp){
+              return { month:ma('right')('quantity').filter({period:rowp('period')}) (0) ('month') }
             })
+          }
         })
+      }).orderBy('name')
         
-        .filter({
-          year:params.year,
-          type_rice_id:params.type_rice_id,
-          ordinal:params.ordinal
+       .filter({
+         year:2560,
+         type_rice_id:'4b23b3af-e292-4ac7-8154-c51363cc5ea7',
+         ordinal:1,
+         status:'nc'
+      })
+      
+        
+        .do(function(all){
+          return {
+            data:all,
+            sum: {
+              amount_all: r.db('eu2').table('allocate').sum('amount'),
+              amount_update_all: r.db('eu2').table('allocate').sum('amount_update'),
+              //p1: r.db('eu2').table('allocate').map(function(sup){ return sup('quantity')('weigth')})       .coerceTo('array')  
+            }
+         }
         })
+
 
         .run().then(function(result){
             res.json(result);
