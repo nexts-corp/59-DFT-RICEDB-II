c9c81120e1173390314a45a6073a1a077580a8ce
chonlatee <chonlatee.s@nexts-corp.com>
2017-01-11 14:52:10 +0700

    notify2

--- app/routes/notify.routes.js
+++ app/routes/notify.routes.js
@@ -1,4 +1,7 @@
-  module.exports=function(app){
+module.exports=function(app){
     var index=require('../controllers/notify/index.controller');
     app.get('/:calculate_id',index.getNotify);
+
+    var notify2IndexCtrl = require('../controllers/notify/notify2.controller');
+    app.get('/notify2',notify2IndexCtrl.selectnotify);
 }
