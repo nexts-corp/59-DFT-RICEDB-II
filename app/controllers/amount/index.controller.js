62df3f9033f9055790c0abe3aba813c904bb1876
chonlatee <chonlatee.s@nexts-corp.com>
2017-01-11 10:00:47 +0700

    edit sumweigth

--- app/controllers/amount/index.controller.js
+++ app/controllers/amount/index.controller.js
@@ -8,54 +8,66 @@ class index{
             params.year = parseInt(params.year);
         }   
 
-      r.db('eu2').table('calculate').innerJoin(r.db('eu2').table('allocate'), function(c,a){
-        return c('id').eq(a('calculate_id'))
-      }).map(function(mr){
-        return mr('right').merge(function(qu){
-          return {ordinal:mr('left')('ordinal')}
-        })
-      })
-        
-        .innerJoin(r.db('eu2').table('exporter'), function(all,e){
-        return all('exporter_id').eq(e('id'))
-      }).map(function(ml){
-        return ml('left').merge(function(mn){
-           return { name:ml('right')('name') }
-        })
-      })
-        
-      .innerJoin(r.db('eu2').table('quota'), function(ta,tq){
-        return ta('quota_id').eq(tq('id'))
-      }).map(function(ma){
-        return ma('left').merge(function(x){
-          return {
-            year:ma('right')('year'), 
-            type_rice_id:ma('right')('type_rice_id'),
-            quantity:ma('left')('quantity').merge(function(rowp){
-              return { month:ma('right')('quantity').filter({period:rowp('period')}) (0) ('month') }
-            })
-          }
-        })
-      }).orderBy('name')
-        
-       .filter({
-         year:2560,
-         type_rice_id:'4b23b3af-e292-4ac7-8154-c51363cc5ea7',
-         ordinal:1,
-         status:'nc'
-      })
-      
-        
-        .do(function(all){
-          return {
-            data:all,
-            sum: {
-              amount_all: r.db('eu2').table('allocate').sum('amount'),
-              amount_update_all: r.db('eu2').table('allocate').sum('amount_update'),
-              //p1: r.db('eu2').table('allocate').map(function(sup){ return sup('quantity')('weigth')})       .coerceTo('array')  
-            }
-         }
-        })
+        r.db('eu2').table('calculate').innerJoin(r.db('eu2').table('allocate'), function(c,a){	
+                return c('id').eq(a('calculate_id'))
+              }).map(function(mr){
+                return mr('right').merge(function(qu){
+                  return {
+                    ordinal:mr('left')('ordinal')
+                    //status_allocate:mr('left')('status')
+                  }
+                })
+              })
+                
+                .innerJoin(r.db('eu2').table('exporter'), function(all,e){
+                return all('exporter_id').eq(e('id'))
+              }).map(function(ml){
+                return ml('left').merge(function(mn){
+                  return { name:ml('right')('name') }
+                })
+              })
+                
+              .innerJoin(r.db('eu2').table('quota'), function(ta,tq){
+                return ta('quota_id').eq(tq('id'))
+              }).map(function(ma){
+                return ma('left').merge(function(x){
+                  return {
+                    year:ma('right')('year'), 
+                    type_rice_id:ma('right')('type_rice_id'),
+                    quantity:ma('left')('quantity').merge(function(rowp){
+                      return { month:ma('right')('quantity').filter({period:rowp('period')}) (0) ('month') }
+                    })
+                  }
+                })
+              }) .orderBy('name') 
+                
+            
+              .filter({
+                year:params.year,
+                type_rice_id:params.type_rice_id,
+                ordinal:params.ordinal
+                //status:'nc',
+                //status_allocate:'n'
+              }) 
+              
+                
+            .do(function(all){
+                  return {
+                    data:all,
+                    sum:  {
+                      sum_period: r.db('eu2').table('quota').filter({type_rice_id:'4b23b3af-e292-4ac7-8154-c51363cc5ea7',year:2560})('quantity')(0)('period')
+                            .map(function(p){
+                                return {
+                                  period: p,
+                                  sum_weigth:all('quantity').map(function(a){ return a.filter({period:p})(0)('weigth') }).sum(),
+                                  sum_weigth_update:all('quantity').map(function(a){ return a.filter({period:p})(0)('weigth_update') }).sum()
+                                }
+                            }),
+                      sum_amount :r.db('eu2').table('allocate').sum('amount'),
+                      sum_amount_update :r.db('eu2').table('allocate').sum('amount_update')
+                  }
+                }
+              })
 
 
         .run().then(function(result){
