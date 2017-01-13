ddbcadc08f1e2951c6bc6a791028c48851ef217e
chonlatee <chonlatee.s@nexts-corp.com>
2017-01-13 16:23:07 +0700

    update

--- app/routes/confirm.routes.js
+++ app/routes/confirm.routes.js
@@ -2,6 +2,7 @@ module.exports = function(app){
 var confirmIndexCtrl = require('../controllers/confirm/index.controller')
     app.get('/confirmordinal',confirmIndexCtrl.selectOrinal);
     app.get('/confirmexporter',confirmIndexCtrl.selectexporter); 
+    app.get('/confirmnoexport',confirmIndexCtrl.selectall_noexporter);  
     app.get('/confirm',confirmIndexCtrl.selectall);  
     app.put('/confirm',confirmIndexCtrl.updateconfirm);
 
