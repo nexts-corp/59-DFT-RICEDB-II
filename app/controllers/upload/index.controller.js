399dc07bc86d3931981cf7e14a2edfab482cf806
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2017-01-16 19:24:49 +0700

    update  upload file

---
+++ app/controllers/upload/index.controller.js
@@ -0,0 +1,98 @@
+var fs = require('fs');
+var path = require('path');
+var multiparty = require('multiparty');
+var stream = require('stream');
+
+class index{
+
+    uploadFile(req,res){
+        
+        var r = req._r;
+        var params = req.params;
+
+        var form = new multiparty.Form();
+        form.parse(req, function (err, fields, files) {
+
+            var prefile = files.file[0];
+            console.log(prefile);
+            
+            fs.readFile(prefile.path, function (err, data) {
+                //console.log(r);
+                r.db('files').table('files').insert({
+                    name: prefile.originalFilename,
+                    type: prefile.headers['content-type'],
+                    contents: data
+                })
+                .run().then(function (result) {
+                    res.json(result);
+                }).catch(function (err) {
+                    res.json(err);
+                })
+            });
+
+
+            //res.json({ec:'01252'});
+        });
+        
+    }
+
+
+    listFile(req,res){
+        
+        var r = req._r;
+        var params = req.params;
+
+        r.db('files').table('files').without('contents').map(function(row){
+            return { name: row('name'), progress: 100, complete: true , file_id:row('id')}
+        })
+        .run().then(function (result) {
+            res.json(result);
+        }).catch(function (err) {
+            res.json(err);
+        })
+        
+    }
+
+    downloadFile(req,res){
+        var r = req._r;
+        var params = req.params;
+        // console.log(params)
+
+        r.db('files').table('files').get(params.id)
+        .run().then(function (result) {
+            res.writeHead(200, {
+                'Content-Type': result.type,
+                'Content-Length': result.contents.length,
+                //'Content-Disposition':'filename='+cursor.name
+                'Content-Disposition': 'attachment; filename=' + result.name
+            });
+
+            var bufferStream = new stream.PassThrough();
+            bufferStream.end(result.contents);
+            bufferStream.pipe(res);
+
+        }).catch(function (err) {
+            res.json(err);
+        })
+
+    }
+
+
+    deleteFile(req,res){
+        var r = req._r;
+        var params = req.params;
+        // console.log(params)
+
+        r.db('files').table('files').get(params.id).delete()
+        .run().then(function (result) {
+            res.json(result);
+        }).catch(function (err) {
+            res.json(err);
+        })
+
+    }
+    
+
+}
+
+module.exports = new index();
---
+++ app/routes/upload.routes.js
@@ -0,0 +1,7 @@
+module.exports = function(app){
+var indexCtrl = require('../controllers/upload/index.controller')
+    app.post('/file',indexCtrl.uploadFile); 
+    app.get('/file/:id',indexCtrl.downloadFile); 
+    app.delete('/file/:id',indexCtrl.deleteFile); 
+    app.get('/list',indexCtrl.listFile); 
+}
