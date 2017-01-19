932314d7045d67c4374f0152213ec13767771230
chonlatee <chonlatee.s@nexts-corp.com>
2017-01-19 17:47:29 +0700

    update

--- app/controllers/confirm/index.controller.js
+++ app/controllers/confirm/index.controller.js
@@ -280,115 +280,7 @@ class index {
                     .do(function(c){return { data:c, c_amount:c('amount').sum() }})  
                 }
             }) // end do
-/*
-            r.db('eu2').table('allocate').innerJoin(r.db('eu2').table('calculate'), function(a,c){
-                return a('calculate_id').eq(c('id'))
-            }).map(function(ml){
-                return ml('left').merge(function(mr){
-                    return { ordinal: ml('right')('ordinal'), status_calculte: ml('right')('status')}
-                })
-            })
-                
-            .innerJoin(r.db('eu2').table('exporter'), function(ta,e){
-                return ta('exporter_id').eq(e('id'))
-            }).map(function(ml){
-                return ml('left').merge(function(mr){
-                    return {name:ml('right')('name')}
-                })
-            })
-                
-            .innerJoin(r.db('eu2').table('quota'), function(ta,q){
-                return ta('quota_id').eq(q('id'))
-            }).map(function(ml){
-                return ml('left').merge(function(mr){
-                    return {
-                        type_rice_id: ml('right')('type_rice_id'),
-                        quantity: ml('left')('quantity').merge(function(m){
-                        return {
-                            month: ml('right')('quantity').filter({period:m('period')})(0)('month'),
-                            percent: ml('right')('quantity').filter({period:m('period')})(0)('percent')
-                        }
-                    }),
-                        year:ml('right')('year')
-                    }
-                })
-            })
-                
-            .filter({
-                year:params.year,
-                type_rice_id:params.type_rice_id,
-                ordinal:params.ordinal,
-                exporter_id:params.exporter_id,
-                status:'nc',
-                status_calculte:'n'
-            }).orderBy('name')
-                
-            .do(function(result){
-                return {
-                notconfirm:result ,
-                confirm : 
-                    r.db('eu2').table('confirm')
-                    .innerJoin(r.db('eu2').table('quota'), function(all,q){
-                            return all('quota_id').eq(q('id'))
-                        }).map(function(x){
-                        return x('left').merge(function(quan){
-                        return {
-                            year:x('right')('year'),
-                            type_rice_id:x('right')('type_rice_id'),
-                            quantity:x('left')('quantity').merge(function(xx){
-                                return {
-                                month: x('right')('quantity').filter({period:xx('period')})(0)('month'),
-                                percent: x('right')('quantity').filter({period:xx('period')})(0)('percent')
-                                }
-                            })
-                        }
-                        })
-                        })
-                
-                    .filter({
-                        year:params.year,
-                        type_rice_id:params.type_rice_id
-                    })
 
-                    
-                    .innerJoin(r.db('eu2').table('exporter'), function(c,e){
-                    return c('exporter_id').eq(e('id'))
-                    }).map(function(ml){
-                    return ml('left').merge(function(x){
-                        return {
-                        name : ml('right')('name')
-                        }
-                    })
-                    })
-                    
-                    .innerJoin(r.db('eu2').table('allocate'), function(ta,al){
-                        return ta('allocate_id').eq(al('id'))
-                    }).map(function(ml){
-                        return ml('left').merge(function(x){
-                        return {
-                            calculate_id:ml('right')('calculate_id')
-                        }
-                        })
-                    })
-                    
-                    .innerJoin(r.db('eu2').table('calculate'), function(ta,ca){
-                        return ta('calculate_id').eq(ca('id'))
-                    }).map(function(ml){
-                        return ml('left').merge(function(x){
-                        return {
-                            ordinal:ml('right')('ordinal')
-                        }
-                        })
-                    })
-  
-                    .filter(function(x){
-                        return x('ordinal').eq(params.ordinal)
-                        .and(x('status').eq('c').or(x('status').eq('r')))
-                        .and(x('exporter_id').eq(params.exporter_id))
-                    }).orderBy('name')
-                }
-            }) // end do
-            */
                 .run().then(function(result){
                     res.json(result);
                 })
@@ -397,43 +289,47 @@ class index {
                 }) 
         }
 
+
         updateconfirm(req,res){
             var r = req._r;
-            var params = req.body;
+            var params = req.body;          
 
-            r.db('eu2').table('allocate').get(params.allocate_id).update({
-                status:'c'
-            }).do(function(result){
-                return r.db('eu2').table('confirm').insert({
-                    amount:params.amount,
-                    exporter_id:params.exporter_id,
-                    quantity:params.quantity,
-                    quota_id:params.quota_id,
-                    allocate_id:params.allocate_id,
+            console.log(params[0].id);
+
+            for (let value of params) {
+                console.log(value.id);
+            
+
+                r.db('eu2').table('allocate').get(value.allocate_id).update({
                     status:'c'
+                }).do(function(result){
+                    return r.db('eu2').table('confirm').insert({
+                        amount:value.amount,
+                        exporter_id:value.exporter_id,
+                        quantity:value.quantity,
+                        quota_id:value.quota_id,
+                        allocate_id:value.allocate_id,
+                        status:'c'
+                    })
                 })
-            })
 
-            .run().then(function(result){
-                res.json(result);
-            })
-            .catch(function(err){
-                res.json(err);
-            })
-
+                .run().then(function(result){
+                    res.json(result);
+                })
+                .catch(function(err){
+                    res.json(err);
+                })
+            }//end for
         }
 
+
+
         deleteconfirm(req,res){
+            var i
             var r = req._r;
             var params = req.body;
-            console.log(params.allocate_id);
-            
-            // let iterable = [10, 20, 30];
-
-            // for (let value of iterable) {
-            //     console.log(value);
-            // }
-
+    
+    
             r.db('eu2').table('allocate').get(params.allocate_id ).update({
                 status:'nc'
             }).do(function(result){
@@ -445,7 +341,11 @@ class index {
             .catch(function(err){
                 res.json(err);
             })
+
+
         }
+
+
 
         tranfer(req,res){
             var r = req._r;
