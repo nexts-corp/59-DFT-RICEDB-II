910c9b492e7b30b4361113f7258c0350e14b6733
Namkhaeng PC-NEXT <pongpanot.t@nexts-corp.com>
2017-01-25 10:50:07 +0700

    update getReqEcList add exporter name

--- app/controllers/payment/payment.controller.js
+++ app/controllers/payment/payment.controller.js
@@ -10,7 +10,15 @@ class Payment{
             year:quotaYear,
             exporter_id:params.exporter_id,
             status:'np'
-        }).coerceTo('array')
+        })
+        .innerJoin(r.db('eu2').table('exporter'),function(left,right){
+            return left('exporter_id').eq(right('id'))
+        })
+        .map(function(row){
+            return row('left').merge(function(row2){
+                return {exporter_name:row('right')('name')}
+            })
+        })
         .run().then(function(result){
             res.json(result);
         })
